d4c9ae97c4f4395ba17ac5f9e2a425296ea941c5
davidinmichael <davidinmichael@gmail.com>
2024-10-14 11:15:49 +0100

    Chat now working

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Chats(models.Model):
+    message = models.TextField()
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from urllib import response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import ChatSerializer
+from .models import Chats
+
+
+class ChatsView(APIView):
+
+    def get(self, request):
+        chats = Chats.objects.all()
+        serializer = ChatSerializer(chats, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = ChatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
